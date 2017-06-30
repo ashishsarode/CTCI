a173e5c2b3bc94cf6adfb2163ac8ab69dcfb7a4d
poonamphate <poonamphate07@gmail.com>
2017-06-30 14:56:46 -0700

    add string

---
+++ StringUniqueChars/src/StringUniqueCharsDemo.java
@@ -0,0 +1,12 @@
+
+public class StringUniqueCharsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//greetings
+		System.out.println("Hello");
+		String str = "Poonam";
+		System.out.println(str);
+	}
+
+}

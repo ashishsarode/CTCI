4b8ce8bc1fa3a382b1c8d1548060850a7956a005
poonamphate <poonamphate07@gmail.com>
2017-06-30 17:32:15 -0700

    Added functionality to method

--- stringPermutation/src/stringPermutation/stringPermutation.java
+++ stringPermutation/src/stringPermutation/stringPermutation.java
@@ -1,7 +1,30 @@
+/*Given two strings, write a method to decide if one is a permutation of the
+other.*/
+
+/*Solution: sort the two strings and then compare using equals() method*/
+
 package stringPermutation;
+import java.util.*;
 
 public class stringPermutation {
 public static void main(String[] args){
-	System.out.println("hi");
+	String str1 = "god";
+	String str2 = "dog ";
+	System.out.println("Input strings are : "+str1+" and "+str2);
+	
+	if (sortString(str1).equals(sortString(str2))){
+		System.out.println("Given two strings are anagrams of each other");
+	}
+	
+	else{
+		System.out.println("Given two strings are not anagrams of each other");
+	}
 }
+	
+	public static String sortString(String str){
+		char[] charArr = str.toCharArray();
+		Arrays.sort(charArr);
+		return new String(charArr);
+	}
 }
+
